878c023260399c62c970903ac580612655a79bd6
Kevin <jcurias123@gmail.com>
2024-02-08 11:22:34 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+
+const App = () => {
+  return (
+    <main>
+        <div>
+            <div className="flex items-center justify-center h-screen">
+                <div className="fade-in text-center">
+                    <h1 className="text-4xl">Something big is happening...</h1>
+                    <h2>a brand new social media world</h2>                    
+                </div>
+
+            </div>
+        </div>
+    </main>
+  );
+}
+
+export default App;
